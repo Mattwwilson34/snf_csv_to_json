608258f4bdfa004d2e21b145967e04b1bb12e98b
Matt Wilson <mattwwilson34@gmail.com>
2023-05-20 20:58:25 -0400

    refactor(app): make output formating dynamic

--- app.py
+++ app.py
@@ -38,45 +38,76 @@ OUTPUT_COLUMNS = [
     "Total Number of Penalties",
 ]
 
-# List of column names to use in the output
-OUTPUT_COLUMN_NAMES = [
-    "name",
-    "address",
-    "num_beds" "residents_per_day",
-    "overall_rating",
-    "num_deficiencies",
-    "num_penalties",
-]
+# Dictionary used to format the output
+# Output will be a list of dictionaries
+# Each dictionary will represent a facility
+# Each key in this dictionary will represent a key in the output dictionary
+# Each value array will represent the columns in the CSV file that will be concatenated into a single string
+OUTPUT_COLUMN_NAMES = {
+    "name": ["Provider Name"],
+    "address": [
+        "Provider Address",
+        "Provider City",
+        "Provider State",
+        "Provider Zip Code",
+    ],
+    "num_beds": ["Number of Certified Beds"],
+    "residents_per_day:": ["Average Number of Residents per Day"],
+    "overall_rating": ["Overall Rating"],
+    "num_deficiencies": ["Rating Cycle 1 Total Number of Health Deficiencies"],
+    "num_penalties": ["Total Number of Penalties"],
+}
 
-
-def format_address(row, output_dict):
-    """Formats the output address into a single string"""
-    address = row[output_dict["Provider Address"]]
-    city = row[output_dict["Provider City"]]
-    state = row[output_dict["Provider State"]]
-    zip_code = row[output_dict["Provider Zip Code"]]
-    return f"{address}, {city}, {state} {zip_code}"
+EMPTY_ROWS = 0
 
 
 def format_output_dictionaries(row, output_dict):
     """Formats the output dictionaries to match the expected output format"""
-    return {
-        "name": row[output_dict["Provider Name"]],
-        "address": format_address(row, output_dict),
-        "num_beds": row[output_dict["Number of Certified Beds"]],
-        "residents_per_day": row[output_dict["Average Number of Residents per Day"]],
-        "overall_rating": row[output_dict["Overall Rating"]],
-        "num_deficiencies": row[output_dict["Rating Cycle 1 Total Number of Health Deficiencies"]],
-        "num_penalties": row[output_dict["Total Number of Penalties"]],
-    }
+
+    output = {}
+    for key, value in OUTPUT_COLUMN_NAMES.items():
+        output[key] = ""
+        for index, column in enumerate(value):
+            if index == 0 or index == len(value) - 1:
+                output[key] += f"{row[output_dict[column]]}"
+            else:
+                output[key] += f"{row[output_dict[column]]}, "
+    return output
 
 
 def get_index_dict_from_columns(columns):
     """Returns a dictionary with column headers as the keys and initial values of None"""
+
     index_dict = {}
     for column in columns:
         index_dict[column] = None
     return index_dict
+
+
+def set_index_dict_from_headers(index_dict, headers, columns):
+    """Sets the index of each column in the index_dict to the index of the column in the headers"""
+
+    for column in columns:
+        for header in headers:
+            if column == header:
+                index_dict[column] = headers.index(header)
+    return index_dict
+
+
+def validate_index_dict_update(index_dict, columns):
+    """Validates that the index_dict was updated with the index of each column"""
+
+    if None in index_dict.values():
+        raise Exception(
+            f"Error: Invalid CSV file. The columns in the CSV file do not match those expected.\n"
+            f"Please ensure that the CSV file contains the following columns: {columns}"
+        )
+
+
+def get_valid_number(value, default):
+    """Returns the value if it is a valid number, otherwise returns the default value"""
+
+    return int(value) if value.isdigit() else default
 
 
 # Dictionary to store the index of each column to filter by
@@ -90,85 +121,43 @@ with codecs.open(FILE, "r", encoding="latin1") as csv_file:
     reader = csv.reader(csv_file, delimiter=",")
     csv_file_headers = next(reader)
 
-    # Find and store the index of each column to filter by
-    for filter_column in FILTER_COLUMNS:
-        for header in csv_file_headers:
-            if filter_column == header:
-                filter_dict[filter_column] = csv_file_headers.index(header)
+    # Update the filter_dict with the index of each column to filter by
+    filter_dict = set_index_dict_from_headers(
+        filter_dict, csv_file_headers, FILTER_COLUMNS
+    )
 
-    # Check if all filter_columns were found in the CSV indicating that the file is valid
-    if None in filter_dict.values():
-        print(
-            """Error: Invalid CSV file. The coumns in the CSV file do not match those expected.
-              Please ensure that the CSV file contains the following columns:
-              Provider State, Overall Rating, Number of Certified Beds, 
-              Rating Cycle 1 Total Number of Health Deficiencies, Total Number of Penalties"""
-        )
-        sys.exit(1)
+    # Throw error if any of the filter_columns were not found in the CSV
+    validate_index_dict_update(filter_dict, FILTER_COLUMNS)
 
-    # Find and store the index of each column to include in the output
-    for output_column in OUTPUT_COLUMNS:
-        for header in csv_file_headers:
-            if output_column == header:
-                output_dict[output_column] = csv_file_headers.index(header)
+    # Update the output_dict with the index of each column to include in the output
+    output_dict = set_index_dict_from_headers(
+        output_dict, csv_file_headers, OUTPUT_COLUMNS
+    )
 
-    # Check if all output_columns were found in the CSV indicating that the file is valid
-    if None in output_dict.values():
-        print(
-            """Error: Invalid CSV file. The coumns in the CSV file do not match those expected.
-            Please ensure that the CSV file contains the following columns:
-            Provider Name, Provider Address, Provider City, Provider State, 
-            Provider Zip Code, Number of Certified Beds, Average Number of Residents Per Day,
-            Overall Rating, Rating Cycle 1 Total Number of Health Deficiencies, Total Number of Penalties"""
-        )
-        sys.exit(2)
+    # Throw error if any of the output_columns were not found in the CSV
+    validate_index_dict_update(output_dict, OUTPUT_COLUMNS)
 
-    # Create a list to store rows of csv data
     filtered_csv_data = []
 
     # Iterate through each row in the CSV file and filter the data
     for index, row in enumerate(reader):
-        # skip empty rows
+        # Skip empty rows
         if len(row) == 0:
+            EMPTY_ROWS += 1
             continue
 
-        # extract header indexs from the filter_dict
-        state_header_index = filter_dict["Provider State"]
-        overall_rating_header_index = filter_dict["Overall Rating"]
-        num_beds_header_index = filter_dict["Number of Certified Beds"]
-        num_deficiencies_header_index = filter_dict[
-            "Rating Cycle 1 Total Number of Health Deficiencies"
-        ]
-        num_penalties_header_index = filter_dict["Total Number of Penalties"]
-
-        # extract data to compare to command line arguments
-        state = row[state_header_index]
-
-        """convert csv string values to integers or floats if posisble otherwise set to default values
-        TODO: consider proper values to set if string is not parsable this will effect weighting of the data
-        """
-        overall_rating = (
-            int(row[overall_rating_header_index])
-            if row[overall_rating_header_index].isdigit()
-            else 1
+        # Get cell values from the row for command line args comparison
+        state = row[filter_dict["Provider State"]]
+        overall_rating = get_valid_number(row[filter_dict["Overall Rating"]], 1)
+        num_beds = get_valid_number(row[filter_dict["Number of Certified Beds"]], 0)
+        num_deficiencies = get_valid_number(
+            row[filter_dict["Rating Cycle 1 Total Number of Health Deficiencies"]], 0
         )
-        num_beds = (
-            int(row[num_beds_header_index])
-            if row[num_beds_header_index].isdigit()
-            else 0
-        )
-        num_deficiencies = (
-            float(row[num_deficiencies_header_index])
-            if row[num_deficiencies_header_index].isdigit()
-            else 0
-        )
-        num_penalties = (
-            float(row[num_penalties_header_index])
-            if row[num_penalties_header_index].isdigit()
-            else 0
+        num_penalties = get_valid_number(
+            row[filter_dict["Total Number of Penalties"]], 0
         )
 
-        # filter the data
+        # Validate that the row meets the filter criteria
         if (
             state == STATE
             and overall_rating >= MIN_OVERALL_RATING
@@ -180,3 +169,5 @@ with codecs.open(FILE, "r", encoding="latin1") as csv_file:
             filtered_csv_data.append(format_output_dictionaries(row, output_dict))
 
 pprint.pprint(len(filtered_csv_data))
+pprint.pprint(filtered_csv_data[0])
+print(f"Empty Rows: {EMPTY_ROWS}")
